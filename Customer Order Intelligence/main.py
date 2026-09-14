c79cf04a2984a3ea042f9b196134466c709c2d6d
TMathidi111 <161381076+TMathidi111@users.noreply.github.com>
2026-09-14 16:19:53 +0200

    2026-09-14

--- Customer Order Intelligence/main.py
+++ Customer Order Intelligence/main.py
@@ -9,11 +9,11 @@
 # Customer, Proudct and Category will need dynamic dictionaries also Province.
 
 import csv
-
 filename = "Customer Order Intelligence/Data/orders.csv"
 valid_transactions = 0
 invalid_transactions = 0
 customer_data = {}
+transaction_log = []
 
 with open(filename,"r") as csvfile:
     for line in csv.DictReader(csvfile):
@@ -23,21 +23,28 @@ with open(filename,"r") as csvfile:
         product = line.get("product")
         category = line.get("category")
         quantity = line.get("quantity")
+        transaction_id = line.get("order_id")
 
          # Validation rules 
-        if price.isdecimal() and quantity.isnumeric():
+         # Try block idea came from chatgpt but i found how to use it properly using W3Schools
+        try :
             price = float(price)
             quantity = int(quantity)
-            revenue = price*quantity
-            valid_transactions += 1
 
-        else :
+            if price > 0 and quantity > 0 and transaction_id not in transaction_log and customer != "":
+                revenue = price*quantity
+                valid_transactions += 1
+                transaction_log.append(transaction_id)
+            else : 
+                invalid_transactions += 1
+
+        except ValueError :
             invalid_transactions += 1
             continue
 
 
         # Dictionaries within dictionaries : data model 
-        # Line 31-43 is from ChatGPT
+        # Line 44-50 is from ChatGPT
         if customer not in customer_data:
             customer_data[customer] = { #We need to create a dictionary for the dictionary
                 "revenue": 0,
@@ -52,6 +59,7 @@ with open(filename,"r") as csvfile:
 
 print(valid_transactions)
 print(invalid_transactions)
-
-        
+# Prints the internal dictionary for each customer 
+for customer in customer_data: 
+    print(customer_data[customer])
         
